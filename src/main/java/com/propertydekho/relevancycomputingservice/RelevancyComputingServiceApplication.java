358e73105adeed0f90b445eed35d5f6624e6ce8c
Divsjains6021 <divsjains6021@gmail.com>
2020-05-10 00:29:13 +0530

    Property relevancy computing service

---
+++ src/main/java/com/propertydekho/relevancycomputingservice/RelevancyComputingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.propertydekho.relevancycomputingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelevancyComputingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelevancyComputingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/propertydekho/relevancycomputingservice/RelevancyComputingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propertydekho.relevancycomputingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelevancyComputingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
